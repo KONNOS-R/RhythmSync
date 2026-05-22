c5d550b1dae84118c8f4a2055e9d901343d587d3
konnosr <konnosr123@gmail.com>
2026-05-22 16:56:12 +0300

    improved converter module

--- rhythmsync/converter.py
+++ rhythmsync/converter.py
@@ -1,4 +1,6 @@
 import subprocess
+from rich.console import Console
+console = Console()
 
 
 #converts audio file to other formats
@@ -6,16 +8,31 @@ def convert(input_file, output_file):
     try:
         subprocess.run([
             "ffmpeg",
-            "-y",
-            "-i", input_file,
-            output_file
-        ], check=True)
 
-        print(f"Converted successfully:")
+            "-y",
+
+            "-i", str(input_file),
+
+            "-map_metadata", "0",
+
+            str(output_file)
+
+        ],
+
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+
+        check=True
+        )
+
+        console.print("[green]Converted successfully:")
         print(f"{input_file} -> {output_file}")
 
     except FileNotFoundError:
-        print("FFmpeg is not installed.")
+        print("Conversion error: FFmpeg is not installed.")
 
     except subprocess.CalledProcessError:
-        print("Conversion failed.")
+        print("Conversion error: Conversion failed.")
+
+    except Exception as e:
+        print(f"Conversion error: {e}")
