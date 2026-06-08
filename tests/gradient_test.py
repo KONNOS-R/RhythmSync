7fe899f24f3cb1e0d76988aa3d02b21597616da0
KONNOS-R <konnosr123@gmail.com>
2026-06-08 23:42:18 +0300

    Final gradient function. I tried to implement it on the player module for the lyrics but it didnt work as expected and discarded changes

--- tests/gradient_test.py
+++ tests/gradient_test.py
@@ -1,30 +1,3 @@
-# gradient function
-# should go from color a to color b in set increments
-
-'''
-def hex_gradient(hex_a, hex_b, increment):
-    HEX_A = [x for x in hex_a if x != "#"]
-    print(HEX_A)
-
-    red_a = (HEX_A[0] + HEX_A[1])
-    green_a = (HEX_A[2] + HEX_A[3])
-    blue_a = (HEX_A[4] + HEX_A[5])
-    print(red_a, green_a, blue_a)
-
-
-    HEX_B = [x for x in hex_b if x != "#"]
-    print(HEX_B)
-
-    #tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-
-
-
-
-hex_gradient("#000000", "#ffffff", 4)
-'''
-
-
-
 def hex_gradient(color1, color2, steps):
     def hex_to_rgb(hex_color):
         hex_color = hex_color.lstrip('#')
@@ -33,16 +6,12 @@ def hex_gradient(color1, color2, steps):
     def rgb_to_hex(rgb):
         return  f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
 
-
-
     rgb1 = hex_to_rgb(color1)
     rgb2 = hex_to_rgb(color2)
 
     steps_r = (rgb2[0] - rgb1[0]) / (steps - 1)
     steps_g = (rgb2[1] - rgb1[1]) / (steps - 1)
     steps_b = (rgb2[2] - rgb1[2]) / (steps - 1)
-
-
 
     gradient = []
     for i in range(steps):
@@ -53,21 +22,17 @@ def hex_gradient(color1, color2, steps):
     
     return gradient
 
-#print(hex_gradient("#000000", "#ffffff", 4))
-
-a = hex_gradient("#ff0000", "#0099FF", 60)
 
 
+
+
+
+"""
 from rich.console import Console
 console = Console()
 
-for i in range(60):
-    console.print(f"[{a[i]}]█",end="")
 
-print("\n")
-
-
-
+#logos
 if True:
     logo = r'''[bold]
  _____  _           _   _                _____                  
@@ -96,8 +61,6 @@ else:
 
 
 
-
-
 lines = logo.splitlines()
 
 length = len(lines[1])
@@ -107,5 +70,6 @@ print(gradient)
 for i in range(1, len(lines)):
     for j in range(length):
         
-        print(f"[{gradient[j]}]{lines[i][j]}", end="")
+        console.print(f"[{gradient[j]}]{lines[i][j]}", end="")
     console.print("\n",end="")
+"""
